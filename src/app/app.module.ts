7db75a3771b18c26afe8b90dac0417a1ddec2637
Ayyaz Zafar <contact@ayyaztech.com>
2020-10-04 18:41:21 +0500

    installed angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
