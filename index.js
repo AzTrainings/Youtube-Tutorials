d61ab50fe72cb95a51cdfe58f71030250c7fea33
Ayyaz Zafar <contact@ayyaztech.com>
2020-10-10 23:07:24 +0500

    Created basic hello world app in express.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello World 3333');
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+app.get('/', (req, res) => {
+  res.send(`
+   <div style="text-align:center; padding-top:40px;">
+   
+   <h1>Welcome to My Website</h1>
+   <p>This is a Hello World app</p>
+   
+   </div>
+  `);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
